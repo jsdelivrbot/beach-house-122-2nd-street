2df04f17ceaeb8ca1dc54ffeab80ee2ab7424ee5
Emran Talukder <emran_talukder@cable.comcast.com>
2017-08-19 20:35:39 -0400

    first version complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var nodemailer  = require('nodemailer');
 var app = express();
 var bodyParser = require('body-parser');
 
@@ -14,21 +15,35 @@ app.use(express.static(__dirname + '/app'));
 
 app.use(bodyParser.json());
 
+var transport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: '122.2nd.street@gmail.com',
+        pass: 'comcast_2017'
+    }
+});
+
 app.post('/book', function(req, res) {
     console.log(req.body);
     var data = req.body;
 
-    var send = require('gmail-send')({
-        user: '122.2nd.street@gmail.com',
-        pass: 'comcast_2017',
-        to: 'byrnec87@gmail.com',
+    var mailOptions = {
+        from: '122.2nd.street@gmail.com',
+        to: 'tnbbccj@aol.com',
+        bcc: 'byrnec87@gmail.com,noriebyrne@yahoo.com',
         subject: 'New Booking Request',
-        text: `name: ${data.name}\nemail: ${data.email}\nphone: ${data.phone}\ncheck-in: ${data.checkIn}\ncheck-out: ${data.checkOut}`
-    }, function(err, eresy) {
-        res.send('success!');    
+        text: `name: ${data.name}\nemail: ${data.email}\nphone: ${data.phone}\ncheck-in: ${data.checkIn}\ncheck-out: ${data.checkOut}`,
+    };
+
+    transport.sendMail(mailOptions, function(err,info) {
+        if(err) {
+            console.log(err);
+            res.json({status: 'error', data: err});
+        } else {
+            console.log('message sent!... ' + info.response);
+            res.json({status: 'success', data: info.response});
+        }
     });
-
-
 });
 
 app.listen(app.get('port'), function() {
